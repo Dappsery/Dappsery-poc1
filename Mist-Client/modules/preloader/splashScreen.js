f880cd3d0babb219968cb7697e0b6bc004eedd1d
Rick Mac Gillis <rick.macgillis@rickmacgillis.com>
2015-11-14 15:03:23 -0500

    Removed migrated code
    Signed-off-by: Rick Mac Gillis <rick.macgillis@rickmacgillis.com>

--- Mist-Client/modules/preloader/splashScreen.js
+++
@@ -1,5 +0,0 @@
-const ipc = require('ipc');
-
-window.ipc = ipc;
-window.mode = location.hash.replace('#splashScreen_','');
-window.dirname = __dirname.replace('modules/preloader','').replace('modules\\preloader','');
