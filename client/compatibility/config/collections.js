58528f3856aa9bc595fca560a2cddd12de1c398c
Rick Mac Gillis <rick.macgillis@rickmacgillis.com>
2015-11-20 17:51:22 -0500

    cozylife-4.meteor.com
    Signed-off-by: Rick Mac Gillis <rick.macgillis@rickmacgillis.com>

--- client/compatibility/config/collections.js
+++ client/compatibility/config/collections.js
@@ -5,3 +5,4 @@
 MarketPlaceAds = new Mongo.Collection(null);
 Publishers = new Mongo.Collection(null);
 MarketPlaceCategories = new Mongo.Collection(null);
+AccountDB = new Mongo.Collection(null);
--- client/templates/views/Account/login.js
+++ client/templates/views/Account/login.js
@@ -3,7 +3,6 @@ if (Meteor.isClient) {
 	Template.login.events({
 		'click #submit-login': function (e) {
 			e.preventDefault();
-			console.log($('#username').val());
 		}
 	});
 	
